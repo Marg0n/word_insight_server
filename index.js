2a9fcddad51730eb28c3fe08924865d2212b665c
MarGoN <monondcosta@gmail.com>
2024-05-11 20:28:39 +0600

    solved mongoDB connection error by using

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 
 //connection to mongodb
 
-const uri = "mongodb+srv://DB_USER:DB_PASS@cluster0.pqvcpai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pqvcpai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
