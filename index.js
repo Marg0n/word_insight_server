e934842b9137afc19125354069aa15b7a87ac5a2
MarGoN <monondcosta@gmail.com>
2024-05-11 19:16:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+//config
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors({
+    origin: [
+        'http://localhost:5000',
+        'http://localhost:5173',
+        'https://worldinsight.netlify.app'
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200,
+}));
+app.use(express.json());
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Server is running');
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+  });
+
+
+//connection to mongodb
